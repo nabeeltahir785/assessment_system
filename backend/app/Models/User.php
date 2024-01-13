1d310b1662abbce60655a7bf675c0fb727bb6304
muhammadnabeelmehmood <nabeel@coredirection.com>
2024-01-13 21:41:44 +0500

    Backend: Designed database schema and established their relationships.

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -42,4 +42,22 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    public function hasRole($role)
+    {
+        return $this->roles()->where('name', $role)->exists();
+    }
+
+    public function hasPermission($permission)
+    {
+        return $this->roles()->whereHas('permissions', function ($query) use ($permission) {
+            $query->where('name', $permission);
+        })->exists();
+    }
 }
